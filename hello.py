2efee1cb82291db9102f356f3105679510017281
Emily Wotruba <emily.wotruba@rwth-aachen.de>
2024-06-08 23:41:53 +0200

    getting set of countries from `pycountry` now

--- hello.py
+++ hello.py
@@ -23,47 +23,9 @@ def result():
 @app.route("/show", methods = ['GET'])
 def show():
     if request.method == 'GET':
-        countries = {
-            "AF", "AL", "DZ", "AD", "AO",
-            "AG", "AR", "AM", "AU", "AT",
-            "AZ", "BS", "BH", "BD", "BB",
-            "BY", "BE", "BZ", "BJ", "BT",
-            "BO", "BA", "BW", "BR", "BN",
-            "BG", "BF", "BI", "CV", "KH",
-            "CM", "CA", "CF", "TD", "CL",
-            "CN", "CO", "KM", "CG", "CR",
-            "HR", "CU", "CY", "CZ", "KP",
-            "CD", "DK", "DJ", "DM", "DO",
-            "EC", "EG", "SV", "GQ", "ER",
-            "EE", "SZ", "ET", "FJ", "FI",
-            "FR", "GA", "GM", "GE", "DE",
-            "GH", "GR", "GD", "GT", "GN",
-            "GW", "GY", "HT", "HN", "HU",
-            "IS", "IN", "ID", "IR", "IQ",
-            "IE", "IL", "IT", "JM", "JP",
-            "JO", "KZ", "KE", "KI", "KW",
-            "KG", "LA", "LV", "LB", "LS",
-            "LR", "LY", "LI", "LT", "LU",
-            "MG", "MW", "MY", "MV", "ML",
-            "MT", "MH", "MR", "MU", "MX",
-            "FM", "MC", "MN", "ME", "MA",
-            "MZ", "MM", "NA", "NR", "NP",
-            "NL", "NZ", "NI", "NE", "NG",
-            "MK", "NO", "OM", "PK", "PW",
-            "PA", "PG", "PY", "PE", "PH",
-            "PL", "PT", "QA", "KR", "MD",
-            "RO", "RU", "RW", "KN", "LC",
-            "VC", "WS", "SM", "ST", "SA",
-            "SN", "RS", "SC", "SL", "SG",
-            "SK", "SI", "SB", "SO", "ZA",
-            "SS", "ES", "LK", "SD", "SR",
-            "SE", "CH", "SY", "TW", "TJ",
-            "TZ", "TH", "TL", "TG", "TO",
-            "TT", "TN", "TR", "TM", "TV",
-            "UG", "UA", "AE", "GB", "US",
-            "UY", "UZ", "VU", "VE", "VN",
-            "YE", "ZM", "ZW"
-        }
+        countries = set()
+        for country in pycountry.countries:
+            countries.add(country.alpha_2)
         id = request.args.get("id")
         data = offers_for_countries(id, countries=countries)
         out = {}
